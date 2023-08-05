06c08e0fcd9fc7b54e39d008a1782bb39928ee5a
Michael Pakhmurin <mdpakhmurin@gmail.com>
2023-08-05 09:16:34 +0700

    Errors prints as default text with red color

--- TFM-LUANCHER.py
+++ TFM-LUANCHER.py
@@ -3,14 +3,18 @@ from Bundle import Bundle
 from Bundler import Bundler
 
 import sys
+from colorama import Fore
 
 launcher = WindowsLauncher()
 bundler = Bundler()
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "run":
-        path = "./"
-        if len(sys.argv) > 2:
-            path = sys.argv[2]
-        bundle = bundler.bundle(path)
-        launcher.launch(bundle)
+    try:
+        if len(sys.argv) > 1 and sys.argv[1] == "run":
+            path = "./"
+            if len(sys.argv) > 2:
+                path = sys.argv[2]
+            bundle = bundler.bundle(path)
+            launcher.launch(bundle)
+    except Exception as ex:
+        print(f"{Fore.RED}{ex}{Fore.RESET}")
